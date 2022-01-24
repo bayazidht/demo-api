3843b2cd0779575b038ce5a78bf3d3fad840692f
bayazidht <bayazidht@hotmail.com>
2022-01-24 21:47:28 +0600

    Demo API

---
+++ app/src/main/java/com/bayazidht/demoapi/MainActivity.java
@@ -0,0 +1,44 @@
+package com.bayazidht.demoapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv);
+
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://reqres.in/api/users/2";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                textView.setText(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText(error.toString());
+            }
+        });
+
+        queue.add(stringRequest);
+
+    }
+}
